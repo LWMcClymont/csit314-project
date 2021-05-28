13f886505649d26f40caae5c2b6ad6cdc563d45f
cactuspoint <pollywaffle101@gmail.com>
2021-05-28 12:15:49 +1000

    simple-calc example that uses api being tested

---
+++ SimpleCalc/simple-calc.py
@@ -0,0 +1,13 @@
+from api import api
+
+def addition(additionString):
+    return api(additionString)
+
+def subtraction(subtractionString):
+    return api(subtractionString)
+
+def multiplication(multiplicationString):
+    return api(multiplicationString)
+
+def division(divisionString):
+    return api(divisionString)
